f0f615ff3cd94a6234e5fd6116f629ba5de6d07f
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-19 02:15:22 +0200

    Printing data

---
+++ Problems/Printing a magic letter/src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        System.out.println("********\n" +
+                "      **\n".repeat(3) +
+                "**    **\n".repeat(2) +
+                " *******");
+    }
+}
