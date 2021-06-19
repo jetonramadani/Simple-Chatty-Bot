a5e279370f4f0e765f6723d651888fc28698ec2a
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-19 16:07:42 +0200

    Types and variables

---
+++ Problems/Assignments (1)/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+
+        System.out.println(a + " " + c + " " + b);
+    }
+}
